9553bfcbc6fe4c902f7be82625b88bf1902c5e3b
Nebunamirax <nebunamirax@gmail.com>
2026-05-17 19:58:56 +0200

    Use normal map tiles and readable clusters

--- src/app.js
+++ src/app.js
@@ -475,9 +475,9 @@ function initMap() {
     zoomControl: true
   });
 
-  L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
   markerLayer = L.layerGroup().addTo(map);
